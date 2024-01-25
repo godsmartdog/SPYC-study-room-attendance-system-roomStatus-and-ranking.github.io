bd7c3e0f2d1ef946062ce0fad76ae3a391f9ad3e
godsmartdog <132557080+godsmartdog@users.noreply.github.com>
2024-01-25 16:33:17 +0800

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,3 +1,4 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 const supabaseUrl = 'https://puisbpdboykphyeexnrh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw';
 const supabase = createClient(supabaseUrl, supabaseKey);
