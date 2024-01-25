f7e49243fe3a62e10a277a47b717ba59130ba538
godsmartdog <132557080+godsmartdog@users.noreply.github.com>
2024-01-25 16:33:50 +0800

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,11 +1,14 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+async function fetchRankingData() {
+
 const supabaseUrl = 'https://puisbpdboykphyeexnrh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const tableBody = document.getElementById('rankingTable').getElementsByTagName('tbody')[0];
 
-async function fetchRankingData() {
+  
   const { data, error } = await supabase
     .from('ranking')
     .select('attendee_id, total_time')
